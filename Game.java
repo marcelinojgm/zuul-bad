107e2f2716fa4c98ed54f3e8c11a93c1aba3f4ae
MJGM <MJGM@example.com>
2015-04-08 12:24:44 +0200

    modificacion del numero de habitaciones existentes y sus conexiones

--- Game.java
+++ Game.java
@@ -34,23 +34,28 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+     
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        
+        Room entrada = new Room("estas en la entrada");
+        Room pasillo = new Room("habitacion 2 segura");
+        Room almacen = new Room("estas en el almacen");
+        Room laboratorio = new Room("estas en el laboratorio");
+        Room despacho = new Room("estas en el despacho");
+        Room banio = new Room("estas en el baño");
+        Room corredor = new Room("estas en el corredor");
+        Room salida = new Room("salida");
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        entrada.setExits(null, pasillo, null , null);
+        pasillo.setExits(despacho, almacen, null, entrada);
+        almacen.setExits(banio, null, null, pasillo);
+        laboratorio.setExits(banio, null, almacen, despacho);
+        despacho.setExits(corredor, laboratorio, pasillo, null);
+        banio.setExits(null,null,laboratorio,corredor);
+        corredor.setExits(null,banio,despacho,salida);
+        salida.setExits(null, corredor, null, null);
 
-        currentRoom = outside;  // start game outside
+        currentRoom = entrada;  // start game outside
     }
 
     /**
